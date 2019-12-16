1df32aab240e22cc64c85e6ea51addacd30a931f
mac-sin <32592314+mac-sin@users.noreply.github.com>
2019-12-16 21:36:36 +0800

    Updates

---
+++ precache-manifest.cce37b60d0543ef8f5f4d1327213ad54.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9930044c31793394defe",
+    "url": "/my-app/static/js/runtime~main.6019dd84.js"
+  },
+  {
+    "revision": "2efd9766fdf805d9307d",
+    "url": "/my-app/static/js/main.79d722cb.chunk.js"
+  },
+  {
+    "revision": "aee3f3bbb2e04cf219d3",
+    "url": "/my-app/static/js/2.4af9707f.chunk.js"
+  },
+  {
+    "revision": "2efd9766fdf805d9307d",
+    "url": "/my-app/static/css/main.add52359.chunk.css"
+  },
+  {
+    "revision": "652f0990e027a1de7368bd02955fa4a2",
+    "url": "/my-app/index.html"
+  }
+];
