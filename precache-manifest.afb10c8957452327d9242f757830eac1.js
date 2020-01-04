e47df29ccc0c7f0ac303e39a0c41010908d125ee
mac-sin <32592314+mac-sin@users.noreply.github.com>
2020-01-04 22:44:46 +0800

    Updates

--- precache-manifest.afb10c8957452327d9242f757830eac1.js
+++ precache-manifest.afb10c8957452327d9242f757830eac1.js
@@ -8,23 +8,23 @@ self.__precacheManifest = [
     "url": "/my-app/static/js/runtime~main.6019dd84.js"
   },
   {
-    "revision": "fc4d2b4a2885acd939ab",
-    "url": "/my-app/static/js/main.6cd5af77.chunk.js"
+    "revision": "82f6fa2811808ac3a2d1",
+    "url": "/my-app/static/js/main.34264f54.chunk.js"
   },
   {
-    "revision": "8ead38220454c226e07a",
-    "url": "/my-app/static/js/2.512ede21.chunk.js"
+    "revision": "903d5ca816cc30ee57b3",
+    "url": "/my-app/static/js/2.78981e7b.chunk.js"
   },
   {
-    "revision": "fc4d2b4a2885acd939ab",
+    "revision": "82f6fa2811808ac3a2d1",
     "url": "/my-app/static/css/main.f3634d27.chunk.css"
   },
   {
-    "revision": "8ead38220454c226e07a",
+    "revision": "903d5ca816cc30ee57b3",
     "url": "/my-app/static/css/2.7c680484.chunk.css"
   },
   {
-    "revision": "6dc299c4c734512589c4d0524fe801a1",
+    "revision": "2f8d2599b4d515bf71b03a4d63e8d499",
     "url": "/my-app/index.html"
   }
 ];
